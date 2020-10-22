7be77e3efedcd19c6e86e16082a336b02b723e9c
Mygod <contact-git@mygod.be>
2020-10-22 17:13:26 -0400

    Reorder tests

--- test/test.js
+++ test/test.js
@@ -7,6 +7,8 @@ const tester = (spawn) => function () {
         rpc = await spawn();
     });
 
+    it('EnumWrapper', () => assert.notStrictEqual(rpc.EnumWrapper, undefined));
+    it('EnumWrapper.InvasionCharacter', () => assert.notStrictEqual(rpc.EnumWrapper.InvasionCharacter.CHARACTER_DRAGON_BALLOON_GRUNT_FEMALE, undefined));
     it('HoloActivityType', () => assert.notStrictEqual(rpc.HoloActivityType.ACTIVITY_CATCH_EXCELLENT_THROW, undefined));
     it('HoloItemCategory', () => assert.notStrictEqual(rpc.HoloItemCategory.ITEM_CATEGORY_RAID_TICKET, undefined));
     it('HoloItemEffect', () => assert.notStrictEqual(rpc.HoloItemEffect.ITEM_EFFECT_FULL_MOTIVATION, undefined));
@@ -18,8 +20,6 @@ const tester = (spawn) => function () {
     it('HoloPokemonMovementType', () => assert.notStrictEqual(rpc.HoloPokemonMovementType.POKEMON_ENC_MOVEMENT_ELECTRIC, undefined));
     it('HoloPokemonType', () => assert.notStrictEqual(rpc.HoloPokemonType.POKEMON_TYPE_FAIRY, undefined));
     it('Item', () => assert.notStrictEqual(rpc.Item.ITEM_REVIVE, undefined));
-    it('EnumWrapper', () => assert.notStrictEqual(rpc.EnumWrapper, undefined));
-    it('EnumWrapper.InvasionCharacter', () => assert.notStrictEqual(rpc.EnumWrapper.InvasionCharacter.CHARACTER_DRAGON_BALLOON_GRUNT_FEMALE, undefined));
     it('PokemonDisplayProto', () => assert.notStrictEqual(rpc.PokemonDisplayProto, undefined));
     it('PokemonDisplayProto.Alignment', () => assert.notStrictEqual(rpc.PokemonDisplayProto.Alignment.PURIFIED, undefined));
     it('PokemonDisplayProto.Costume', () => assert.notStrictEqual(rpc.PokemonDisplayProto.Costume.NOT_FOR_RELEASE_ALPHA, undefined));
